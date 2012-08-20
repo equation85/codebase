4658ae741ec41ac596a38df526116b765701b0be
Frank Chen <frank85chen@gmail.com>
2012-08-20 21:37:33 +0800

    bug fix: when source or target is null character

--- LevenshteinDistance/LevenshteinDistance.R
+++ LevenshteinDistance/LevenshteinDistance.R
@@ -15,6 +15,9 @@ levenshtein.distance <- function(source, target, type=c('distance','matrix'), in
 	type <- match.arg(type)
 	source.vec <- strsplit(source,'')[[1]]
 	target.vec <- strsplit(target,'')[[1]]
+	if(length(source.vec)==0 & length(target.vec)==0) return(0)
+	if(length(source.vec)==0) return(sum(sapply(target.vec,insert.fun)))
+	if(length(target.vec)==0) return(sum(sapply(source.vec,delete.fun)))
 	ns <- length(source.vec) + 1
 	nt <- length(target.vec) + 1
 	d <- matrix(0,nrow=ns,ncol=nt,dimnames=list(c('#',source.vec),c('#',target.vec)))
